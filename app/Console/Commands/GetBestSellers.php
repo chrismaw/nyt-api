4cdfae0ee7dc9fdb9434c479d0a7239edafffc3e
Christopher Maw <cmaw@onesource.net>
2024-11-24 21:45:50 -0500

    init commit

---
+++ app/Console/Commands/GetBestSellers.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Pail\File;
+
+class GetBestSellers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:get-best-sellers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get Complete list of Best Sellers and store in JSON Format';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $path = '/app/private.history.json';
+        $results = [];
+        $count = 0;
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get('https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=' . config('nyt-api.api_key'));
+
+        $json = json_decode($response->getBody()->getContents(), true);
+        $num_results = intval($json['num_results']); // 36488
+
+        $this->info('Number of Results: ' . $num_results);
+//        $total_requests = ceil($num_results/20);
+        $this->info('Total Requests: 5');
+
+        $bar = $this->output->createProgressBar(5); // only limited to 5 requests but 100 is sufficient for project
+        $bar->start();
+
+        $results = array_merge($results, $json['results']); // first 20
+        $count += count($json['results']);
+        $bar->advance();
+
+        while ($count <= 80) { // only want 100 total
+            $response = $client->get('https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?api-key=' . config('nyt-api.api_key') . '&offset=' . $count);
+            $results = array_merge($results, $json['results']); // next 20
+            $count += count($json['results']);
+            $bar->advance();
+        }
+
+        Storage::put('history.json',json_encode($results),'private'); // overwrites if file already exists
+        $this->newLine(1);
+        $this->info($count . 'books stored' . PHP_EOL);
+        return true;
+
+    }
+}
---
+++ config/nyt-api.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'api_key' => env('NYT_API_KEY', ''),
+];
