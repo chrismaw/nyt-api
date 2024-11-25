157e2a2019005d5d5a197837f3526d050134d3f8
Christopher Maw <cmaw@onesource.net>
2024-11-24 23:48:33 -0500

    testing validation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/api/1/nyt')->group(function () {
+        Route::get('best-sellers', [BookController::class, 'getBestSellers']);
 });
