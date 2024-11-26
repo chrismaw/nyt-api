e044200eeb2e5c67c74b083d68c60ab2530a6da2
Christopher Maw <cmaw@onesource.net>
2024-11-26 16:55:37 -0500

    working on isbn filter

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,9 +14,8 @@ class BookController extends Controller
 
         $author = $validated['author'] ?? null;
         $title = $validated['title'] ?? null;
-        $isbn[] = $validated['isbn[]'] ?? null;
+        $isbns = $validated['isbn'] ?? null;
         $offset = $validated['offset'] ?? 0;
-
         $path = 'history.json';
 
         // Check if the file exists
@@ -24,11 +23,27 @@ class BookController extends Controller
             return response()->json(['error' => 'File not found'], 404);
         }
         // Retrieve and decode JSON data
-        $jsonContent = collect(Storage::disk('app')->json($path));
-        $offsetJSON = $jsonContent->splice($offset, 20);
-        dd($offsetJSON);
-        $books = json_decode($jsonContent, true);
-        $books = Storage::json($filePath);
+        $books = collect(Storage::disk('app')->json($path));
+        $filteredBooks = $books->filter(function ($book) use ($author, $title, $isbns) {
+            $matchesAuthor = !$author || str_contains(strtolower($book['author']), strtolower($author));
+            $matchesTitle = !$title || str_contains(strtolower($book['title']), strtolower($title));
+            $isbnMatches = false;
+            dd($book['isbns'][0]);
+            if ($isbns && count($isbns)) {
+                foreach ($isbns as $isbn) {
+                    $isbnMatches = in_array($isbn, array_values($book['isbns'][0]));
+                }
+            }
+            $matchesISBN = !$isbns || $isbnMatches;
+            return $matchesAuthor && $matchesTitle && $matchesISBN;
+        });
+
+        $booksData = $filteredBooks->splice($offset, 20);
+        dd($booksData);
+        // Return the filtered books
+        return $booksData->values();
+        $books = $jsonContent->splice($offset, 20);
+        dd($booksData);
         if (json_last_error() !== JSON_ERROR_NONE) {
             return response()->json(['error' => 'Error decoding JSON: ' . json_last_error_msg()], 500);
         }
